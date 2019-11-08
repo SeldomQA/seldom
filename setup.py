74706668d5eceb8a874b5755bc92ebc7ada42d95
defnngj <353332934@qq.com>
2019-11-08 11:37:54 +0800

    fix error

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries ::Testing'
     ],
