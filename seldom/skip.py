59c48b9d32fc450598a68f99815b5538366c3e89
fnngj <fnngj@126.com>
2019-12-28 21:03:00 +0800

    skip if

--- seldom/skip.py
+++ seldom/skip.py
@@ -2,6 +2,21 @@ import unittest
 
 
 def skip(reason=None):
+    """
+    Unconditionally skip a test.
+    :param reason:
+    :return:
+    """
     if reason is None:
         reason = "Skip the use case."
     return unittest.skip(reason)
+
+
+def skip_if(condition, reason):
+    """
+    Skip a test if the condition is true.
+    :param condition:
+    :param reason:
+    :return:
+    """
+    return unittest.skipIf(condition, reason)
