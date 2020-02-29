2fa73060ad0e4e8bd39f4003ea526e6f00eade4b
fnngj <fnngj@126.com>
2020-02-29 22:56:08 +0800

    add function excel_to_list()

--- seldom/testdata/conversion.py
+++ seldom/testdata/conversion.py
@@ -4,24 +4,59 @@ Data type conversion of different files
 import csv
 import codecs
 from itertools import islice
+try:
+    from openpyxl import load_workbook
+except ModuleNotFoundError:
+    raise ModuleNotFoundError("Please install the library. https://pypi.org/project/openpyxl/")
 
 
-def csv_to_list(file=None, line=None):
+def csv_to_list(file=None, line=1):
     """
     Convert CSV file data to list
     :param file: Path to file
-    :param line: Start line of read data, default to start at line 0
+    :param line: Start line of read data
     :return:  list data
+
+    @data(csv_to_list(/home/user/seldom/data.csv, line=1))
+    def test_login(self, username, password):
+        print(username)
+        print(password)
     """
     if file is None:
         raise FileExistsError("Please specify the CSV file to convert.")
 
-    if line is None:
-        line = 0
-
-    data_list = []
+    table_data = []
     csv_data = csv.reader(codecs.open(file, 'r', 'utf_8_sig'))
-    for line in islice(csv_data, line, None):
-        data_list.append(line)
+    for line in islice(csv_data, line - 1, None):
+        table_data.append(line)
 
-    return data_list
+    return table_data
+
+
+def excel_to_list(file=None, sheet="Sheet1", line=1):
+    """
+    Convert CSV file data to list
+    :param file: Path to file
+    :param sheet: excel sheet, default name is Sheet1
+    :param line: Start line of read data
+    :return: list data
+
+    @data(excel_to_list(/home/user/seldom/data.xlsx, sheet="Sheet1", line=1))
+    def test_login(self, username, password):
+        print(username)
+        print(password)
+    """
+    if file is None:
+        raise FileExistsError("Please specify the Excel file to convert.")
+
+    excel_table = load_workbook(file)
+    sheet = excel_table[sheet]
+
+    table_data = []
+    for line in sheet.iter_rows(line, sheet.max_row):
+        line_data = []
+        for field in line:
+            line_data.append(field.value)
+        table_data.append(line_data)
+
+    return table_data
