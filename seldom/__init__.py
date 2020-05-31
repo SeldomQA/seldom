240180f81a6c74e6b7e6d6e8d21689f113045acc
defnngj <353332934@qq.com>
2020-05-31 18:20:10 +0800

    v 1.5.3

--- seldom/__init__.py
+++ seldom/__init__.py
@@ -30,6 +30,6 @@ from .skip import skip_unless
 
 __author__ = "bugmaster"
 
-__version__ = "1.5.2"
+__version__ = "1.5.3"
 
 __description__ = "WebUI automation testing framework based on Selenium."
