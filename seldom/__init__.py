e52905ab7ee436914984ae8ea981ae3db6ae4323
defnngj <defnngj@gmail.com>
2021-05-28 23:12:54 +0800

    version: 2.1.1

--- seldom/__init__.py
+++ seldom/__init__.py
@@ -31,6 +31,6 @@ from .request import HttpRequest
 
 __author__ = "bugmaster"
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 
 __description__ = "WebUI/HTTP automation testing framework based on unittest."
