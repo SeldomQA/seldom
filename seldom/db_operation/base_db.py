49d1e82bb1be3c2ec3c15a9c3050d2195f853bb5
defnngj <defnngj@gmail.com>
2021-07-26 23:40:06 +0800

    feat: db add delete/insert/select/update API

--- seldom/db_operation/base_db.py
+++ seldom/db_operation/base_db.py
@@ -22,3 +22,27 @@ class SQLBase:
             tmp = "{k}='{v}'".format(k=key, v=value)
             tmp_list.append(tmp)
         return ' and '.join(tmp_list)
+
+    def delete(self, table, where=None):
+        """
+        delete table data
+        """
+        return self.delete_data(table, where)
+
+    def insert(self, table, data):
+        """
+        insert sql statement
+        """
+        return self.insert_data(self, table, data)
+
+    def select(self, table, where=None):
+        """
+        select sql statement
+        """
+        return self.select_data(table, where)
+
+    def update(self, table, data, where):
+        """
+        update sql statement
+        """
+        return self.update_data(self, table, data, where)
