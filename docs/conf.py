579f667442387b8d198cf438ff8564df41cbf261
zhiheng <zhiheng.hu@klook.com>
2021-11-08 17:16:36 +0800

    version: 2.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'seldom'
 year = '2015-2021'
 author = 'bugmaster'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '2.2.3'
+version = release = '2.3.2'
 
 pygments_style = 'trac'
 templates_path = ['.']
