b3353ea115dd00a18c53d422cdd3ef161528987f
zhiheng <zhiheng.hu@klook.com>
2022-01-21 19:20:00 +0800

    feat: test main extend

---
+++ seldom/running/loader_extend.py
@@ -0,0 +1,56 @@
+"""seldom Loading unittests."""
+
+import functools
+from fnmatch import fnmatchcase
+from unittest.loader import TestLoader
+
+
+class SeldomTestLoader(TestLoader):
+    """
+    This class is responsible for loading tests according to various criteria
+    and returning them wrapped in a TestSuite
+    """
+    testNamePatterns = None
+    collectCaseInfo = False  # Switch of collecting use case information
+    collectCaseList = []     # List of use case information
+
+    def getTestCaseNames(self, testCaseClass):
+        """Return a sorted sequence of method names found within testCaseClass
+        """
+
+        def shouldIncludeMethod(attrname):
+            if not attrname.startswith(self.testMethodPrefix):
+                return False
+            testFunc = getattr(testCaseClass, attrname)
+            # print("testFunc", testFunc)
+            if not callable(testFunc):
+                return False
+            fullName = f'%s.%s.%s' % (
+                testCaseClass.__module__, testCaseClass.__qualname__, attrname
+            )
+            if self.collectCaseInfo is True:
+                case_info = {
+                    "file": testCaseClass.__module__,
+                    "class": {
+                        "name": testCaseClass.__name__,
+                        "doc": testCaseClass.__doc__
+                    },
+                    "function": {
+                        "name": attrname,
+                        "doc": testFunc.__doc__
+                    }
+                }
+                self.collectCaseList.append(case_info)
+
+            return self.testNamePatterns is None or \
+                   any(fnmatchcase(fullName, pattern) for pattern in self.testNamePatterns)
+
+        testFnNames = list(filter(shouldIncludeMethod, dir(testCaseClass)))
+        # print("testFnNames", testFnNames)
+        if self.sortTestMethodsUsing:
+            testFnNames.sort(key=functools.cmp_to_key(self.sortTestMethodsUsing))
+        return testFnNames
+
+
+seldomTestLoader = SeldomTestLoader()
+
--- seldom/running/next_runner.py
+++ seldom/running/next_runner.py
@@ -11,6 +11,7 @@ from seldom.driver import Browser
 from seldom.running.DebugTestRunner import DebugTestRunner
 from seldom.running.HTMLTestRunner import HTMLTestRunner
 from seldom.running.config import Seldom, BrowserConfig
+from seldom.running.loader_extend import seldomTestLoader
 from selenium.webdriver.remote.webdriver import WebDriver as SeleniumWebDriver
 
 INIT_FILE = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "__init__.py")
@@ -34,9 +35,11 @@ class TestMain(object):
     """
     Reimplemented Seldom Runner, Support for Web and API
     """
+    TestSuits = []
+
     def __init__(self, path=None, browser=None, base_url=None, debug=False, timeout=10,
                  report=None, title="Seldom Test Report", description="Test case execution",
-                 rerun=0, save_last_run=False, whitelist=[], blacklist=[]):
+                 rerun=0, save_last_run=False, whitelist=[], blacklist=[], auto=True):
         """
         runner test case
         :param path:
@@ -49,12 +52,14 @@ class TestMain(object):
         :param timeout:
         :param rerun:
         :param save_last_run:
-        :param whitelist
-        :param blacklist
+        :param whitelist:
+        :param blacklist:
+        :param auto:
         :return:
         """
         print(seldom_str)
         self.path = path
+        self.browser = browser
         self.report = report
         self.title = title
         self.description = description
@@ -74,9 +79,9 @@ class TestMain(object):
         Seldom.debug = debug
         Seldom.base_url = base_url
 
-        # Global launch browser
-        if browser is not None:
-            BrowserConfig.NAME = browser
+        # ----- Global launch browser -----
+        if self.browser is not None:
+            BrowserConfig.NAME = self.browser
             Seldom.driver = Browser(BrowserConfig.NAME)
 
         if self.path is None:
@@ -91,28 +96,29 @@ class TestMain(object):
                 this_file = file_path.split("/")[-1]
             else:
                 this_file = file_path
-            suits = unittest.defaultTestLoader.discover(file_dir, this_file)
+            self.TestSuits = seldomTestLoader.discover(file_dir, this_file)
         else:
             if len(self.path) > 3:
                 if self.path[-3:] == ".py":
                     if "/" in self.path:
                         path_list = self.path.split("/")
                         path_dir = self.path.replace(path_list[-1], "")
-                        suits = unittest.defaultTestLoader.discover(path_dir, pattern=path_list[-1])
+                        self.TestSuits = seldomTestLoader.discover(path_dir, pattern=path_list[-1])
                     else:
-                        suits = unittest.defaultTestLoader.discover(os.getcwd(), pattern=self.path)
+                        self.TestSuits = seldomTestLoader.discover(os.getcwd(), pattern=self.path)
                 else:
-                    suits = unittest.defaultTestLoader.discover(self.path)
+                    self.TestSuits = seldomTestLoader.discover(self.path)
             else:
-                suits = unittest.defaultTestLoader.discover(self.path)
+                self.TestSuits = seldomTestLoader.discover(self.path)
 
-        self._run_test_case(suits)
+        if auto is True:
+            self.run(self.TestSuits)
 
-        # Close browser globally
+        # ----- Close browser globally -----
         if isinstance(Seldom.driver, SeleniumWebDriver):
             Seldom.driver.quit()
 
-    def _run_test_case(self, suits):
+    def run(self, suits):
         """
         run test case
         """
@@ -147,6 +153,71 @@ class TestMain(object):
                 verbosity=2)
             runner.run(suits)
             log.printf("A run the test in debug mode without generating HTML report!")
+
+
+class TestMainExtend(TestMain):
+    """
+    TestMain tests class extensions.
+    1. Collect use case information and return to the list
+    2. Execute the use cases based on the use case list
+    """
+
+    def __init__(self, path=None, browser=None, base_url=None, debug=False, timeout=10,
+                 report=None, title="Seldom Test Report", description="Test case execution",
+                 rerun=0, save_last_run=False, whitelist=[], blacklist=[], auto=False):
+
+        if path is None:
+            raise FileNotFoundError("Specify a file path")
+
+        super().__init__(path=path, browser=browser, base_url=base_url, debug=debug, timeout=timeout,
+                         report=report, title=title, description=description,
+                         rerun=rerun, save_last_run=save_last_run, whitelist=whitelist, blacklist=blacklist,
+                         auto=auto)
+
+    @staticmethod
+    def _diff_case(file_name: str, class_name: str, function_name: str, data: list) -> bool:
+        """
+        Diff use case information
+        :param file_name:
+        :param class_name:
+        :param function_name:
+        :param data:
+        :return:
+        """
+        for d in data:
+            d_file = d["file"]
+            d_class = d["class"]["name"]
+            d_function = d["function"]["name"]
+            if file_name == d_file and class_name == d_class and function_name == d_function:
+                return True
+
+        return False
+
+    def run_case(self, data: list) -> None:
+        """
+        run list case
+        :param data:
+        :return:
+        """
+        if isinstance(data, list) is False:
+            raise TypeError("Use cases must be lists.")
+
+        if len(data) == 0:
+            log.error("There are no use cases to execute")
+            return
+
+        suit = unittest.TestSuite()
+        for suits in self.TestSuits:
+            for cases in suits:
+                for case in cases:
+                    file_name = case.__module__
+                    class_name = case.__class__.__name__
+                    function_name = str(case).split(" ")[0]
+                    ret = self._diff_case(file_name, class_name, function_name, data)
+                    if ret is True:
+                        suit.addTest(case)
+
+        self.run(suit)
 
 
 main = TestMain
