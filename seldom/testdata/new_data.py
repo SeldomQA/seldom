b45fa3d56ddb187520e161610381c8fe1c302716
defnngj <defnngj@gmail.com>
2022-02-19 00:42:26 +0800

    feat: new test data

---
+++ seldom/testdata/new_data.py
@@ -0,0 +1,38 @@
+
+"""
+https://www.ssa.gov/oact/babynames/decades/names2010s.html
+"""
+
+first_names_male = """
+noah liam jacob william mason ethan michael alexander james elijah benjamin daniel aiden logan jayden
+matthew lucas david jackson joseph anthony samuel joshua gabriel andrew john christopher olive dylan carter
+isaac luke henry owen ryan nathan wyatt caleb sebastian jack christian jonathan julian landon levi
+isaiah hunter aaron charles thomas eli jaxon connor nicholas jeremiah grayson cameron brayden adrian evan
+jordan josiah angel robert gavin tyler austin colton jose dominic brandon lan lincoln hudson kevin
+zachary adam mateo jason chase nolan ayden cooper parker xavier asher carson jace easton justin
+leo bentley jaxson nathaniel blake elias theodore kayden luis tristan ezra bryson juan brody vincent
+micah miles santiago cole ryder carlos damian leonardo roman max sawyer jesus diego greyson alex
+maxwell axel eric wesley declan giovanni ezekiel braxton ashton ivan hayden camden silas bryce weston
+harrison jameson george antonio timothy kaiden jonah everett miguel steven richard emmett victor kaleb kai
+maverick joel bryan maddox kingston aidan patrick edward emmanuel jude alejandro preston luca bennett jesse
+colin jaden malachi kaden jayce alan kyle marcus brian ryker grant jeremy abel riley calvin
+brantley caden oscar abraham brady sean jake tucker nicolas mark amir avery king gael kenneth
+bradley cayden xander graham rowan
+"""
+
+first_names_female = """
+emma olivia sophia isabella ava mia abigail emily charlotte madison elizabeth amelia evelyn ella chloe
+harper avery sofia grace addison victoria lily natalie aubrey lillian zoey hannah layla brooklyn scarlett
+zoe camila samantha riley leah aria savannah audrey anna allison gabriella claire hailey penelope aaliyah
+sarah nevaeh kaylee stella mila nora ellie bella alexa lucy arianna violet ariana genesis alexis
+elanor maya caroline peyton skylar madelyn serenity kennedy taylor alyssa autumn paisley ashley brianna sadie
+naomi kylie julia sophie mackenzie eva gianna luna katherine hazel khloe ruby melanie piper lydia
+aubree madeline aurora faith alexandra alice kayla jasmine maria annabelle lauren reagan elena rylee isabelle
+bailey eliana sydney makayla cora morgan natalia kimberly vivian quinn valentina andrea willow clara london
+jade liliana jocelyn kinsley trinity brielle mary molly hadley delilah emilia josephine brooke lvy lilly
+adeline payton lyla isla jordyn paige isabel mariah mya nicole valeria destiny rachel ximena emery
+everly sara angelina adalynn kendall reese aliyah margaret juliana melody amy eden mckenzie laila vanessa
+ariel gracie valerie adalyn brooklynn gabrielle kaitlyn athena elise jessica adriana leilani ryleigh daisy nova
+norah eliza rose rebecca michelle alaina catherine londyn summer lila jayla katelyn daniela harmony alana
+alana amaya emerson julianna cecilia Izabella
+"""
