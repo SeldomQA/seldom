13ca05d27ece0ec9db8d2903407317b06009c09c
defnngj <defnngj@gmail.com>
2022-04-16 11:44:36 +0800

    ✨  create project

--- seldom/cli.py
+++ seldom/cli.py
@@ -130,15 +130,15 @@ def create_scaffold(project_name):
         log.info(msg)
 
     test_data = '''{
- "baidu":  [
+ "bing":  [
     ["case1", "seldom"],
     ["case2", "poium"],
-    ["case3", "HTMLTestRunner"]
+    ["case3", "XTestRunner"]
  ]
 }
 
 '''
-    test_sample = '''import seldom
+    test_web_sample = '''import seldom
 from seldom import file_data
 
 
@@ -152,12 +152,11 @@ class SampleTest(seldom.TestCase):
 
 class DDTTest(seldom.TestCase):
 
-    @file_data(file="data.json", key="baidu")
+    @file_data(file="data.json", key="bing")
     def test_data_driver(self, _, keyword):
         """ data driver case """
-        self.open("https://www.baidu.com")
-        self.type(id_="kw", text=keyword)
-        self.click(css="#su")
+        self.open("https://cn.bing.com")
+        self.type(id_="sb_form_q", text=keyword, enter=True)
         self.assertInTitle(keyword)
 
 
@@ -165,12 +164,44 @@ if __name__ == '__main__':
     seldom.main(debug=True)
 
 '''
+    test_api_sample = '''import seldom
+
+
+class TestRequest(seldom.TestCase):
+    """api test case"""
+    
+    def start(self):
+        self.base_url = "http://httpbin.org"
+    
+    def test_put_method(self):
+        self.put(f'{self.base_url}/put', data={'key': 'value'})
+        self.assertStatusCode(200)
+
+    def test_post_method(self):
+        self.post(f'{self.base_url}/post', data={'key':'value'})
+        self.assertStatusCode(200)
+
+    def test_get_method(self):
+        payload = {'key1': 'value1', 'key2': 'value2'}
+        self.get(f'{self.base_url}/get', params=payload)
+        self.assertStatusCode(200)
+
+    def test_delete_method(self):
+        self.delete(f'{self.base_url}/delete')
+        self.assertStatusCode(200)
+
+
+if __name__ == '__main__':
+    seldom.main()
+
+    '''
+
     run_test = """import seldom
 
 
 if __name__ == '__main__':
     # run test file
-    # seldom.main("./test_dir/test_sample.py")
+    # seldom.main("./test_dir/test_web_sample.py")
     # run test dir
     seldom.main("./test_dir/")
 
@@ -180,7 +211,8 @@ if __name__ == '__main__':
     create_folder(os.path.join(project_name, "reports"))
     create_folder(os.path.join(project_name, "test_data"))
     create_file(os.path.join(project_name, "test_data", "data.json"), test_data)
-    create_file(os.path.join(project_name, "test_dir", "test_sample.py"), test_sample)
+    create_file(os.path.join(project_name, "test_dir", "test_web_sample.py"), test_web_sample)
+    create_file(os.path.join(project_name, "test_dir", "test_api_sample.py"), test_api_sample)
     create_file(os.path.join(project_name, "run.py"), run_test)
 
 
