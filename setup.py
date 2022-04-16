43b17ab9dd7e6786ad394cadc12354dfd2d695f5
defnngj <defnngj@gmail.com>
2022-04-16 11:56:54 +0800

    🔧 update XTestRunner version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'selenium>=4.0.0',
-        'XTestRunner>=1.3.2',
+        'XTestRunner>=1.4.1',
         'parameterized==0.8.1',
         'loguru>=0.4.1',
         'openpyxl>=3.0.3',
