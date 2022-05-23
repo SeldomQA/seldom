ba76781c39d609b3587033203e45fcca2f643412
zhiheng <zhiheng.hu@klook.com>
2022-05-23 16:24:56 +0800

    🔧 update webdriver_manager version

--- seldom/utils/webdriver_manager_extend.py
+++ seldom/utils/webdriver_manager_extend.py
@@ -1,42 +1,43 @@
 """
 https://github.com/SergeyPirogov/webdriver_manager
 """
-import logging
 import os
-from webdriver_manager import utils
-from webdriver_manager.driver import ChromeDriver
-from webdriver_manager.logger import log
-from webdriver_manager.manager import DriverManager
-from webdriver_manager.utils import ChromeType
+from webdriver_manager.core import utils
+from webdriver_manager.drivers.chrome import ChromeDriver
+from webdriver_manager.core.manager import DriverManager
+from webdriver_manager.core.utils import ChromeType
 
 
 class ChromeDriverManager(DriverManager):
     """
     Since Google cannot be used in China, Taobao is used to replace Google
     """
-    def __init__(self, version="latest",
+    def __init__(self,
+                 version="latest",
                  os_type=utils.os_type(),
                  path=None,
                  name="chromedriver",
                  url="https://registry.npmmirror.com/-/binary/chromedriver",
                  latest_release_url="https://registry.npmmirror.com/-/binary/chromedriver/LATEST_RELEASE",
                  chrome_type=ChromeType.GOOGLE,
-                 log_level=logging.INFO,
-                 print_first_line=True,
-                 cache_valid_range=1):
-        super().__init__(path, log_level=log_level, print_first_line=print_first_line,
-                         cache_valid_range=cache_valid_range)
+                 cache_valid_range=1,
+                 download_manager=None):
+        super().__init__(
+            path,
+            cache_valid_range=cache_valid_range,
+            download_manager=download_manager)
 
-        self.driver = ChromeDriver(name=name,
-                                   version=version,
-                                   os_type=os_type,
-                                   url=url,
-                                   latest_release_url=latest_release_url,
-                                   chrome_type=chrome_type)
+        self.driver = ChromeDriver(
+            name=name,
+            version=version,
+            os_type=os_type,
+            url=url,
+            latest_release_url=latest_release_url,
+            chrome_type=chrome_type,
+            http_client=self.http_client,
+        )
 
     def install(self):
-        log(f"Current {self.driver.chrome_type} version is {self.driver.browser_version}", first_line=True)
         driver_path = self._get_driver_path(self.driver)
-
         os.chmod(driver_path, 0o755)
         return driver_path
