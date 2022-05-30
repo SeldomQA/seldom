0dfd42be00c7be3f28374d2111270142e69fb216
defnngj <defnngj@gmail.com>
2022-05-30 23:29:21 +0800

    🔖 seldom==2.10.1

--- seldom/__init__.py
+++ seldom/__init__.py
@@ -31,6 +31,6 @@ from .testdata.parameterization import *
 
 __author__ = "bugmaster"
 
-__version__ = "2.10.0"
+__version__ = "2.10.1"
 
 __description__ = "WebUI/HTTP automation testing framework based on unittest."
