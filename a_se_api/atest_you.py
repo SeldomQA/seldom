fbc4aa099596dfdd0019c2aee3ef695a2df8ce46
defnngj <defnngj@gmail.com>
2022-07-25 22:43:22 +0800

    ❌️delete

--- a_se_api/atest_you.py
+++
@@ -1,7 +0,0 @@
-from you_se import YouSe
-
-you = YouSe()
-you.open("https://www.baidu.com")
-you.type(name="wd", text="selenium")
-you.click(id="su")
-you.close()
--- test_sql.py
+++
@@ -1,5 +0,0 @@
-from seldom.db_operation import SQLiteDB
-
-db = SQLiteDB(r"D:\github\quick\backend\dev.sqlite3")
-
-db.select("projects")
