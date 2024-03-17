95431b1a51940da794ceff45b2e03b742df8c6e4
虫师 <defnngj@gmail.com>
2024-03-17 23:37:21 +0800

    🔧 remove support for python 3.7.

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
