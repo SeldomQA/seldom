256d9b162fee720de2cbc76701d956f899da62ff
defnngj <defnngj@gmail.com>
2024-07-06 15:53:26 +0800

    🔖 version: 3.7.1

--- seldom/__init__.py
+++ seldom/__init__.py
@@ -30,6 +30,6 @@ from .testdata.parameterization import *
 
 __author__ = "bugmaster"
 
-__version__ = "3.7.0"
+__version__ = "3.7.1"
 
 __description__ = "Seldom is an automation testing framework based on unittest."
