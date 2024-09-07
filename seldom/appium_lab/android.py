48a07e4f583f1a60a48d1a6a4975bb36afe4df1a
defnngj <defnngj@gmail.com>
2024-09-08 03:35:12 +0800

    🔧 add EspressoOptions.

--- seldom/appium_lab/android.py
+++ seldom/appium_lab/android.py
@@ -1,7 +1,9 @@
 from appium.options.android import UiAutomator2Options
+from appium.options.android import EspressoOptions
 from appium.options.common.base import T
 from typing import Any, Dict
 from seldom.running.config import Seldom
+from seldom.logging import log
 
 
 class UiAutomator2Options(UiAutomator2Options):
@@ -11,8 +13,23 @@ class UiAutomator2Options(UiAutomator2Options):
 
     def load_capabilities(self: T, caps: Dict[str, Any]) -> T:
         """Sets multiple capabilities"""
+        log.info(f"app info: {caps}")
         for name, value in caps.items():
             if name == "appPackage":
                 Seldom.app_package = value
+            self.set_capability(name, value)
+        return self
+
+
+class EspressoOptions(EspressoOptions):
+    """
+    Override EspressoOptions class methods
+    """
+    
+    def load_capabilities(self: T, caps: Dict[str, Any]) -> T:
+        """Sets multiple capabilities"""
+        log.info(f"app info: {caps}")
+        for name, value in caps.items():
+            Seldom.app_package = value
             self.set_capability(name, value)
         return self
