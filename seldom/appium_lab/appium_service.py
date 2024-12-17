a9e594b50f891e47d7ca9ee2976f2d7388e2ddb2
defnngj <defnngj@gmail.com>
2024-12-17 20:29:54 +0800

    :zap: start appium service.

--- seldom/appium_lab/appium_service.py
+++ seldom/appium_lab/appium_service.py
@@ -2,27 +2,36 @@ from appium.webdriver.appium_service import AppiumService as OriginalServer
 
 
 class AppiumService(OriginalServer):
+    """
+    appium service
+    """
 
-    def __init__(self, addr, port='4723'):
+    def __init__(self, addr: str = "127.0.0.1", port: str = "4723"):
+        super().__init__()
         self.addr = addr
         self.port = port
 
-        super().__init__()
+    def start_service(self, **kwargs) -> None:
+        """
+        start service
+        :param kwargs:
 
-    def start_service(self, **kwargs):
-        # args = [
-        #     f'-p {self.port}',
-        #     # f'-g {self.log_file_path}',
-        #     '--session-override',
-        #     '--log-timestamp',
-        #     '--session-override',
-        #     '--local-timezone',
-        #     '--allow-insecure chromedriver_autodownload',
-        # ]
+        args = [
+            f'-p {self.port}',
+            f'-g {self.log_file_path}',
+            '--session-override',
+            '--log-timestamp',
+            '--session-override',
+            '--local-timezone',
+            '--allow-insecure chromedriver_autodownload',
+        ]
+        :return:
+        """
+
         args = ['--address', self.addr, '-p', self.port]
         self.start(args=args, **kwargs)
 
 
 if __name__ == '__main__':
-    service = AppiumService('127.0.0.1')
+    service = AppiumService()
     service.start_service()
