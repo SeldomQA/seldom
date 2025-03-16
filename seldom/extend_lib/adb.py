ddba4342e3238ecc78ad2018f749e6587c4aad03
defnngj <defnngj@gmail.com>
2025-03-16 17:52:05 +0800

    :art: update device API

--- seldom/extend_lib/adb.py
+++ seldom/extend_lib/adb.py
@@ -2,7 +2,7 @@ import os
 import time
 
 
-def get_device_info(device: str) -> dict:
+def get_device_name(device: str) -> str:
     """
     Get device name and serial number.
     :param device:
@@ -10,27 +10,27 @@ def get_device_info(device: str) -> dict:
     """
     try:
         name = os.popen(f"adb -s {device} shell getprop ro.product.model").read().strip().replace(' ', '')
-        return {device: name}
+        return name
     except Exception as e:
         print(f"Error retrieving device info for {device}: {e}")
-        return {}
+        return ""
 
 
-def get_devices() -> dict:
+def get_devices() -> []:
     """
     Get a list of dictionaries, each containing a single device's name and serial number.
     :return:
     """
-    devices = {}
+    devices = []
     raw_devices = os.popen("adb devices").read().splitlines()[1:]  # Skip the first line which is a header
     for line in raw_devices:
         parts = line.split()
         if len(parts) > 1 and parts[1] == "device":
             serial = parts[0]
-            device_info = get_device_info(serial)
+            print(serial)
+            device_info = get_device_name(serial)
             if device_info:
-                # Create a dictionary from the tuple and add it to the list
-                devices = {**devices, **device_info}
+                devices.append((serial, device_info))
     return devices
 
 
